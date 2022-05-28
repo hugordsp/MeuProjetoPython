6438ee378fd41122472090f5dc5460b8d4d00995
HugoRodrigues <hugordsp@gmail.com>
2022-05-28 12:06:00 -0300

    Revert "Mudei o valor da condicional"
    This reverts commit a6ab3df323c7d8604713f70cc6f3dd916a62c93b.

--- teste.py
+++ teste.py
@@ -2,6 +2,11 @@ print('Hello World! ')
 print('Linha 2')
 print('Hugo Rodrigues')
 
-valor = 12
+valor = 10
+<<<<<<< HEAD
 if valor >= 10:
     print('Teste funcionou')
+=======
+if valor <= 10:
+    print('Teste')
+>>>>>>> parent of 3c78424 (Removi a condicional do código)
