b7044caa9c4d7ef59825f7fd5bf058f37f49b18d
HugoRodrigues <hugordsp@gmail.com>
2022-05-28 11:49:51 -0300

    Teste modificação

--- teste.py
+++ teste.py
@@ -1,7 +1,11 @@
 print('Hello World! ')
+<<<<<<< HEAD
 print('Linha 2')
 print('Hugo Rodrigues')
 
 valor = 10
 if valor >= 10:
     print('Teste funcionou')
+=======
+print('Linha 2')
+>>>>>>> parent of 616e866 (Adicionei meu nome ao código)
