b752b8aac35c86c7e80b53e4f0d08ac94ac6fed7
HugoRodrigues <hugordsp@gmail.com>
2022-05-28 12:01:17 -0300

    Voltei a versão

--- teste.py
+++ teste.py
@@ -3,5 +3,10 @@ print('Linha 2')
 print('Hugo Rodrigues')
 
 valor = 10
+<<<<<<< HEAD
 if valor >= 10:
     print('Teste funcionou')
+=======
+if valor <= 10:
+    print('Teste')
+>>>>>>> parent of 3c78424 (Removi a condicional do código)
